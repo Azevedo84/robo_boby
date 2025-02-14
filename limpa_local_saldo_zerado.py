30e451a6fa9c7abcbc555c2935fb32559a12d0c0
Anderson Azevedo <azevedo.anderson@ymail.com>
2025-02-14 17:15:58 -0300

    14/02/25

---
+++ limpa_local_saldo_zerado.py
@@ -0,0 +1,43 @@
+import sys
+from banco_dados.conexao import conecta
+from banco_dados.controle_erros import grava_erro_banco
+import os
+import traceback
+import inspect
+
+
+class LimpaLocalSaldoZerado:
+    def __init__(self):
+        nome_arquivo_com_caminho = inspect.getframeinfo(inspect.currentframe()).filename
+        self.nome_arquivo = os.path.basename(nome_arquivo_com_caminho)
+
+    def trata_excecao(self, nome_funcao, mensagem, arquivo, excecao):
+        try:
+            tb = traceback.extract_tb(excecao)
+            num_linha_erro = tb[-1][1]
+
+            traceback.print_exc()
+            print(f'Houve um problema no arquivo: {arquivo} na função: "{nome_funcao}"\n{mensagem} {num_linha_erro}')
+
+            grava_erro_banco(nome_funcao, mensagem, arquivo, num_linha_erro)
+
+        except Exception as e:
+            nome_funcao_trat = inspect.currentframe().f_code.co_name
+            exc_traceback = sys.exc_info()[2]
+            tb = traceback.extract_tb(exc_traceback)
+            num_linha_erro = tb[-1][1]
+            print(f'Houve um problema no arquivo: {self.nome_arquivo} na função: "{nome_funcao_trat}"\n'
+                  f'{e} {num_linha_erro}')
+            grava_erro_banco(nome_funcao_trat, e, self.nome_arquivo, num_linha_erro)
+
+    def manipula_comeco(self):
+        try:
+            pass
+
+        except Exception as e:
+            nome_funcao = inspect.currentframe().f_code.co_name
+            exc_traceback = sys.exc_info()[2]
+            self.trata_excecao(nome_funcao, str(e), self.nome_arquivo, exc_traceback)
+
+chama_classe = LimpaLocalSaldoZerado()
+chama_classe.manipula_comeco()
